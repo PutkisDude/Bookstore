81321ef004d04342b61d5e0406b0ba3dd7ef6ef2
PutkisDude <putkis@gmail.com>
2021-09-08 23:44:37 +0300

    Homework

---
+++ src/main/java/hh/swd20/Bookstore/web/BookstoreController.java
@@ -0,0 +1,15 @@
+package hh.swd20.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class BookstoreController {
+
+	@GetMapping("/index")
+	public String index(Model model) {
+		return "index";
+	}
+}
