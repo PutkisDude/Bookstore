c88850e3cfd8189ae6ac6483811e4da24e607303
PutkisDude <putkis@gmail.com>
2021-09-28 15:05:16 +0300

    Added category html templates and controller

--- src/main/java/hh/swd20/Bookstore/web/BookstoreController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookstoreController.java
@@ -6,8 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import hh.swd20.Bookstore.domain.Book;
 import hh.swd20.Bookstore.domain.BookRepository;
 
---
+++ src/main/java/hh/swd20/Bookstore/web/CategoryController.java
@@ -0,0 +1,47 @@
+package hh.swd20.Bookstore.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import hh.swd20.Bookstore.domain.Category;
+import hh.swd20.Bookstore.domain.CategoryRepository;
+
+@Controller
+public class CategoryController {
+
+	@Autowired
+	private CategoryRepository catRepository;
+	
+	@GetMapping("/categorylist")
+	public String categoryList(Model model) {
+		model.addAttribute("categories", catRepository.findAll());
+		return "categorylist";
+	}
+	@GetMapping("/deletecategory/{id}")
+	public String deleteCategory(@PathVariable("id") Long catId, Model model) {
+		catRepository.deleteById(catId);
+		return "redirect:../categorylist";
+	}
+	
+	@GetMapping("/addcategory")
+	public String addCategory(Model model) {
+		model.addAttribute("category", new Category());
+		return "addcategory";
+	}
+	
+	@GetMapping("/editcategory/{id}")
+	public String editCategory(@PathVariable("id") Long catId, Model model) {
+		model.addAttribute("category", catRepository.findById(catId).get());
+		return "renamecat";
+	}
+	
+	@PostMapping("/savecat")
+	public String saveCategory(Category category) {
+		catRepository.save(category);
+		return "redirect:categorylist";
+	}
+}
