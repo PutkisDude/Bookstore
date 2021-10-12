16a1c09ede4cff458462d8227e624a5fe53253a8
PutkisDude <putkis@gmail.com>
2021-10-12 23:29:52 +0300

    Some bootstrap n bug fixes

--- src/main/java/hh/swd20/Bookstore/WebSecurityConfig.java
+++ src/main/java/hh/swd20/Bookstore/WebSecurityConfig.java
@@ -21,6 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		http
 			.authorizeRequests()
 			.antMatchers("/css/**").permitAll()
+			.anyRequest().authenticated()
 			.and()
 		.formLogin()
 			.loginPage("/login")
